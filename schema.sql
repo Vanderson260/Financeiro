ecc7544eb7905714e514bf44f29cc042806e1965
Vanderson260 <vandersoncanedocorrea260@gmail.com>
2026-09-07 13:45:38 -0300

    Delete schema.sql

--- schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS app_state (
-  id INTEGER PRIMARY KEY CHECK (id = 1),
-  state JSONB NOT NULL DEFAULT '{}'::jsonb,
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-
-INSERT INTO app_state (id, state)
-VALUES (1, '{}'::jsonb)
-ON CONFLICT (id) DO NOTHING;
